0a71f3292a554a1ced792e09fe081556f7834558
abdullahalmsaodi <abdullah.almsaodi@fastn.ai>
2026-05-18 22:29:15 +0300

    feat(hero): add landing page hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-
 const App = () => {
-    return (
-        <div >
-            <h1 className="text-3xl font-bold ">
-                App
-            </h1>
-                </div>
-    )
-}
-export default App
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You’ll Love
+            Without the Hassle
+          </h1>
+
+          <p>Search</p>
+        </header>
+      </div>
+    </main>
+  );
+};
+export default App;
