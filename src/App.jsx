d0701858b6e042d693ba45b55d9bea2893121d05
abdullahalmsaodi <abdullah.almsaodi@fastn.ai>
2026-05-18 22:01:11 +0300

    chore(assets): add project image resources

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>App</div>
+        <div >
+            <h1 className="text-3xl font-bold ">
+                App
+            </h1>
+                </div>
     )
 }
 export default App
