1ea0f9735065c308c074e55277115a1cc514fbd6
abdullahalmsaodi <abdullah.almsaodi@fastn.ai>
2026-05-19 18:35:29 +0300

    perf(search): optimize movie search with debounce

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Search from "./components/Search.jsx";
 import { useEffect, useState } from "react";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
+import { useDebounce } from "react-use";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_API_KEY;
@@ -17,6 +18,12 @@ const App = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+
+  const [debounceSearchTerm, setDebounceSearchTerm] = useState("");
+
+  // Debounce the search term to prevent making too many API requests
+  // by waiting for the user to stop typing for 500ms
+  useDebounce(() => setDebounceSearchTerm(searchTerm), 500, [searchTerm]);
 
   const fetchMovies = async (query) => {
     setIsLoading(true);
@@ -46,8 +53,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    fetchMovies(searchTerm);
-  }, [searchTerm]);
+    fetchMovies(debounceSearchTerm);
+  }, [debounceSearchTerm]);
   return (
     <main>
       <div className="pattern" />
